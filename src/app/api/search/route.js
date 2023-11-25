c5fec0b37533ba6f0e14444655c311bf546dbe72
NhanPhan159 <nhandut2019@gmail.com>
2023-11-26 01:33:41 +0700

    search

---
+++ src/app/api/search/route.js
@@ -0,0 +1,18 @@
+export async function GET(req, res) {
+    try {
+      // Fetch data from the external API (127.0.0.1:8000 in this case)
+      const url = new URL(req.url)
+      const skip = url.searchParams.get("param")
+        console.log("đá",skip);
+      const response = await fetch(`http://127.0.0.1:8000/search?param=${skip}`);
+      const data = await response.json();
+  
+      // Return the data as JSON
+      console.log("dât",data.data);
+      return Response.json(data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "antd";
 import { Input } from "antd";
-import React, { useEffect, useState, lazy, Suspense  } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 const LazyComponent = lazy(() => import("@/components/products`"));
 
 const getClothes = async () => {
@@ -11,35 +11,70 @@ const getClothes = async () => {
 
   // You need to await or use .then() here
   let data = await response.json();
-  console.log(data);
   return data;
 };
+
+const ChatApi = async (input) => {
+  let response = await fetch("/api/chat", {
+    method: "POST",
+    body: JSON.stringify({ input }),
+  });
+  // You need to await or use .then() here
+  let data = await response.json();
+  return data;
+};
+
+const SearchApi = async (param) => {
+  let response = await fetch(`/api/search?param=${param}`, {
+    method: "GET",
+  });
+  // You need to await or use .then() here
+  let data = await response.json();
+  console.log(data);
+
+  return data;
+};
+
 export default function Home() {
   const [clothes, setClothes] = useState([]);
-  const [showLazy,setShowLazy] = useState(false)
-  const [isSuggest,setIsSuggest] = useState(false)
+  const [showLazy, setShowLazy] = useState(false);
+  const [isSuggest, setIsSuggest] = useState(false);
+  const [input, setInput] = useState("");
+  const [chatResult, setChatResult] = useState("");
 
   // Use a function declaration instead of arrow function
   const fetchData = () => {
     setClothes([]);
-    setShowLazy(false)
+    setShowLazy(false);
     getClothes()
       .then((data) => {
         setClothes(data);
-        setShowLazy(true)
+        setShowLazy(true);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   };
-  const fetchDataSuggest = () =>{
-    setShowLazy(false)
-    setIsSuggest(false)
+  const fetchDataSuggest = () => {
+    setShowLazy(false);
+    setIsSuggest(false);
     //api
-    setShowLazy(true)
-    setIsSuggest(true)
-
-  }
+    ChatApi(
+      input +
+        " choose one in streetwear,elegant,vintage,casual,active,luxury just answear one word and write result in lowercase"
+    )
+      .then((data) => {
+        SearchApi(data).then((data) => {
+          setClothes(data);
+        });
+        setShowLazy(true);
+        setIsSuggest(true);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    //
+  };
 
   return (
     <main className="flex min-h-screen bg-slate-50 flex-col items-center justify-between p-24">
@@ -47,26 +82,37 @@ export default function Home() {
         <div className="flex flex-col w-1/6 text-black gap-3 text-base">
           <p>Link</p>
           <p>Describe your situation</p>
+          {chatResult}
         </div>
         <div className="flex flex-col w-4/6 text-black gap-3">
           <div className="flex gap-2">
             : <Input className="flex-1" placeholder="Insert link heer" />
           </div>
           <div className="flex gap-2">
-            : <Input className="flex-1" placeholder="" />
+            :{" "}
+            <Input
+              className="flex-1"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder=""
+            />
           </div>
         </div>
         <div className="flex flex-col w-1/6 text-black gap-3">
           <Button onClick={fetchData} type="primary" className="bg-red-200">
             Get product
           </Button>
-          <Button onClick={fetchDataSuggest} type="primary" className="bg-red-200">
+          <Button
+            onClick={fetchDataSuggest}
+            type="primary"
+            className="bg-red-200"
+          >
             Suggest
           </Button>
         </div>
       </div>
       <Suspense fallback={<div className="text-black">Loading...</div>}>
-        {showLazy && (<LazyComponent data={clothes} isSuggest={isSuggest}/>)}
+        {showLazy && <LazyComponent data={clothes} isSuggest={isSuggest} />}
       </Suspense>
     </main>
   );
